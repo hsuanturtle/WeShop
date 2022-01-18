443648b19586e9399d19ab7801d8ee7240a8fdb5
Judy Chuang <tw26581129@gamil.com>
2022-01-18 21:54:12 +0800

    add clientId

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { UserProvider } from "./context/user_context";
-//dev-5la21ahl.us.auth0.com
-//q4GftxL7rB0CqYhWqIQCJXCRFGTTCD5w
+
 ReactDOM.render(
   <Auth0Provider
     domain="dev-5la21ahl.us.auth0.com"
-    clientID="q4GftxL7rB0CqYhWqIQCJXCRFGTTCD5w"
+    clientId="q4GftxL7rB0CqYhWqIQCJXCRFGTTCD5w"
     redirectUri={window.location.origin}
   >
     <UserProvider>
